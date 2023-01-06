2deda858bed0b2a3a57b27f1e0cc449613ab8556
uones <uones@DESKTOP-BPDJQNM>
2023-01-06 15:15:12 +0300

    Add project files.

---
+++ Domain/Visitors/Visitor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Visitors
+{
+    public class Visitor
+    {
+        public  string Ip { get; set; }
+        public string CurrentLink { get; set; }
+        public string ReferrerLink { get; set; }
+        public string Method { get; set; }
+        public string Protocol { get; set; }
+        public string PhysicalPath { get; set; }
+        public VisitorVersion Browser { get; set; }
+        public VisitorVersion OperationSystem { get; set; }
+        public Device Device { get; set; }
+
+    }
+}
